959b499b839acdf20969229e1c4c8cb5a4908731
AtharvOP01 <166982410+AtharvOP01@users.noreply.github.com>
2024-04-15 01:11:06 +0530

    Update logs.py

--- HK/modules/logs.py
+++ HK/modules/logs.py
@@ -41,15 +41,15 @@ async def logs(legend):
         start = datetime.now()
         fetch = await legend.reply(f"__Fetching Logs...__")
     
-        with open("HKLogs.txt", "w") as logfile:
-            logfile.write("🦋⃟ᬊᴠ‌ɪ‌ᴘ‌•༎꯭ࠫ𝆺꯭𝅥 ᥫ꯭ 𝗛🇰‌๛❣𓆩ꭙ🇩‌𓆪ꪾ𐏓 ⃪༏‌⃪🔥᪵᪳❛ [ Bot Logs ]\n\n" + logs)
+        with open("PRRMIUMLogs.txt", "w") as logfile:
+            logfile.write("𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ×͜× [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
         await asyncio.sleep(1)
 
         try:
-            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"⚡ **ʜᴋ x sᴘᴀᴍ ʟᴏɢs** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
+            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"⚡ **ᴘʀʀᴍɪᴜᴍ x sᴘᴀᴍ ʟᴏɢs** ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
             await fetch.delete()
         except Exception as e:
             await fetch.edit(f"An Exception Occured!\n\n**ERROR:** {str(e)}")
