2d01da7d9c4df51c284a142d0bbc33d8b3a965b7
AtharvOP01 <166982410+AtharvOP01@users.noreply.github.com>
2024-04-15 01:06:05 +0530

    Update spam.py

--- HK/modules/spam.py
+++ HK/modules/spam.py
@@ -59,10 +59,10 @@ async def spam(event: events):
                     await event.client.send_message(event.chat_id, message)
                     await asyncio.sleep(0.2)
             else:
-                await event.reply(f"😈 **Usage:**\n  » {hl}spam 13 🦋⃟ᬊᴠ‌ɪ‌ᴘ‌•༎꯭ࠫ𝆺꯭𝅥 ᥫ꯭ 𝗛🇰‌๛❣𓆩ꭙ🇩‌𓆪ꪾ𐏓 ⃪༏‌⃪🔥᪵᪳❛\n  » {hl}spam 13 <ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ>\n\n**To do spam with replying to a user:**\n  » {hl}spam 13 🥵 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ>")
+                await event.reply(f"😈 **Usage:**\n  » {hl}spam 13 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ×͜×\n  » {hl}spam 13 <ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ>\n\n**To do spam with replying to a user:**\n  » {hl}spam 13 🥵 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ>")
 
         except (IndexError, ValueError):
-            await event.reply(f"😈 **Usage:**\n  » {hl}spam 13 🦋⃟ᬊᴠ‌ɪ‌ᴘ‌•༎꯭ࠫ𝆺꯭𝅥 ᥫ꯭ 𝗛🇰‌๛❣𓆩ꭙ🇩‌𓆪ꪾ𐏓 ⃪༏‌⃪🔥᪵᪳❛\n  » {hl}spam 13 <ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ>\n\n**To do spam with replying to a user:**\n  » {hl}spam 13 😎 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ>")
+            await event.reply(f"😈 **Usage:**\n  » {hl}spam 13 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ×͜×\n  » {hl}spam 13 <ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ>\n\n**To do spam with replying to a user:**\n  » {hl}spam 13 😎 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ>")
         except Exception as e:
             print(e)
 
@@ -80,7 +80,7 @@ async def spam(event: events):
 async def pspam(event):
     if event.sender_id in SUDO_USERS:
         if event.chat_id in GROUP:
-            await event.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ 🦋⃟ᬊᴠ‌ɪ‌ᴘ‌•༎꯭ࠫ𝆺꯭𝅥 ᥫ꯭ 𝗛🇰‌๛❣𓆩ꭙ🇩‌𓆪ꪾ𐏓 ⃪༏‌⃪🔥᪵᪳❛ ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
+            await event.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ×͜× ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
         else:
             try:
                 counter = int(event.text.split(" ", 2)[1])
@@ -108,7 +108,7 @@ async def pspam(event):
 async def hang(e):
     if e.sender_id in SUDO_USERS:
         if e.chat_id in GROUP:
-            await e.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ 🦋⃟ᬊᴠ‌ɪ‌ᴘ‌•༎꯭ࠫ𝆺꯭𝅥 ᥫ꯭ 𝗛🇰‌๛❣𓆩ꭙ🇩‌𓆪ꪾ𐏓 ⃪༏‌⃪🔥᪵᪳❛ ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
+            await e.reply("» ꜱᴏʀʀʏ, ᴛʜɪꜱ ɪꜱ 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ×͜× ᴘʀᴏᴛᴇᴄᴛᴇᴅ ɢʀᴏᴜᴘ.")
         else:
             try:
                 counter = int(e.text.split(" ", 2)[1])
